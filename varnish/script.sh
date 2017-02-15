5ea83ae03cd210e44b00889ebf6ccdab8772b6e2
Antonio Luna <antonio__luna@outlook.es>
2017-02-15 18:52:38 +0100

    varnish script foreground fix

--- varnish/script.sh
+++ varnish/script.sh
@@ -4,4 +4,4 @@ ipaddr=$(dig +short apache)
 
 sed -i "s/IPAPACHE/$ipaddr/g" /etc/varnish/default.vcl
 
-/usr/sbin/varnishd -a :80 -T localhost:6082 -f /etc/varnish/default.vcl -S /etc/varnish/secret -s malloc,256m
+/usr/sbin/varnishd -F -a :80 -T localhost:6082 -f /etc/varnish/default.vcl -S /etc/varnish/secret -s malloc,256m
