ad5b412881a088f5d4c2e54d380a258d5de30f3f
Antonio Luna <antonio__luna@outlook.es>
2017-02-15 18:35:59 +0100

    varnish and apache fixes

--- varnish/script.sh
+++ varnish/script.sh
@@ -4,4 +4,4 @@ ipaddr=$(dig +short apache)
 
 sed -i "s/IPAPACHE/$ipaddr/g" /etc/varnish/default.vcl
 
-varnishd -F -f /etc/varnish/default.vcl -s malloc,256m -a 0.0.0.0:6081
+ExecStart=/usr/sbin/varnishd -a :80 -T localhost:6082 -f /etc/varnish/default.vcl -S /etc/varnish/secret -s malloc,256m
